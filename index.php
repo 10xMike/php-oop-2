b287027ac557b87bb83292ca5b1b7365af19e383
10xMike <michelegec@gmail.com>
2021-06-28 20:15:45 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l'ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Provate a far interagire tra di loro gli oggetti: ad esempio, l'utente dello shop inserisce una carta di credito...
+*/
+
+class Product {
+    protected $name;
+    protected $desc;
+    protected $price;
+    protected $make;
+
+    public function __construct(string $name, string $desc, float $price, string $make)
+    {
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->price = $price;
+        $this->make = $make;
+    }
+}
+
+?>
